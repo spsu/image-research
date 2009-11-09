0be5df67b9c392991cdec72d064cb1680cac6c21
Brandon Thomas Suit <brandon.suit@gmail.com>
2009-11-09 17:29:21 -0500

    More camera data...
    File descriptor: 3
    Driver: uvcvideo
    Card: Vimicro USB2.0 PC Camera
    Bus info: usb-0000:00:1d.7-7.4
    Version: 256
    
    Capabilities:
    	Capture video
    	Has streaming
    
    Pixel Format Data:
    	width: 640
    	width: 480
    	bytes/line: 1280
    	image buffer size: 614400
    
    	pixel format: YUYV (aka YUV 4:2:2)
    	color space: Unknown code...
    	pixel field: None / progressive or undetermined

--- source/testv4l.cpp
+++ source/testv4l.cpp
@@ -7,18 +7,10 @@
 
 const char* getPixFormat(int fourcc)
 {
+	// I know Minoru is YUYV, so this is first.
 	switch(fourcc) {
-		case V4L2_PIX_FMT_DV:
-		case V4L2_PIX_FMT_ET61X251:
-		case V4L2_PIX_FMT_HI240:
-		case V4L2_PIX_FMT_HM12:
-		case V4L2_PIX_FMT_MJPEG:
-		case V4L2_PIX_FMT_PWC1:
-		case V4L2_PIX_FMT_PWC2:
-		case V4L2_PIX_FMT_SN9C10X:
-		case V4L2_PIX_FMT_WNVA:	
-		case V4L2_PIX_FMT_YYUV:
-			return "Reserved";
+		case V4L2_PIX_FMT_YUYV:
+			return "YUYV (aka YUV 4:2:2)";
 	}
 
 	switch(fourcc) {
@@ -30,14 +22,73 @@ const char* getPixFormat(int fourcc)
 	}
 
 	switch(fourcc) {
-		case V4L2_PIX_FMT_YUYV:
-			return "YUYV (aka YUV 4:2:2)";
+		case V4L2_PIX_FMT_DV:
+		case V4L2_PIX_FMT_ET61X251:
+		case V4L2_PIX_FMT_HI240:
+		case V4L2_PIX_FMT_HM12:
+		case V4L2_PIX_FMT_MJPEG:
+		case V4L2_PIX_FMT_PWC1:
+		case V4L2_PIX_FMT_PWC2:
+		case V4L2_PIX_FMT_SN9C10X:
+		case V4L2_PIX_FMT_WNVA:	
+		case V4L2_PIX_FMT_YYUV:
+			return "Reserved format identifier";
 	}
 
 	return "Unknown";
 }
 
+const char* getField(int f)
+{
+	switch(f) {
+		case V4L2_FIELD_ANY:
+			return "Any";
+		case V4L2_FIELD_NONE:
+			return "None / progressive or undetermined";
+		case V4L2_FIELD_TOP:
+			return "Top";
+		case V4L2_FIELD_BOTTOM:
+			return "Bottom";
+		case V4L2_FIELD_INTERLACED:
+			return "Interlaced";
+		case V4L2_FIELD_SEQ_TB:
+			return "Seq TB";
+		case V4L2_FIELD_SEQ_BT:
+			return "Seq BT";
+		case V4L2_FIELD_ALTERNATE:
+			return "Alternate";
+		case V4L2_FIELD_INTERLACED_TB:
+			return "Interlaced TB";
+		case V4L2_FIELD_INTERLACED_BT:
+			return "Interlaced BT";
+		default:
+			return "Unknown code...\n";
+	}
+}
 
+const char* getColorspace(int c)
+{
+	switch(c) {
+		case V4L2_COLORSPACE_SMPTE170M:
+			return "NSTC/PAL";
+		case V4L2_COLORSPACE_SMPTE240M:
+			return "1125-line US HDTV";
+		case V4L2_COLORSPACE_REC709:
+			return "HDTV and modern devices";
+		case V4L2_COLORSPACE_BT878:
+			return "Broken Bt878 extents";
+		case V4L2_COLORSPACE_470_SYSTEM_M:
+			return "M/NSTC";
+		case V4L2_COLORSPACE_470_SYSTEM_BG:
+			return "625-line PAL and SECAM";
+		case V4L2_COLORSPACE_JPEG:
+			return "JPEG Y'CbCr";
+		case V4L2_COLORSPACE_SRGB:
+			return "?";
+		default:
+			return "Unknown code...";
+	}
+}
 
 int main(int argc, char* argv[])
 {
@@ -81,12 +132,19 @@ int main(int argc, char* argv[])
 	}
 
 
-	printf("Pixel Data:\n");
+	printf("Pixel Format Data:\n");
 	printf("\twidth: %d\n", format.fmt.pix.width);
 	printf("\twidth: %d\n", format.fmt.pix.height);
+	printf("\tbytes/line: %d\n", format.fmt.pix.bytesperline);
+	printf("\timage buffer size: %d\n", format.fmt.pix.sizeimage);
 
 	pixfmt = format.fmt.pix.pixelformat;
-	printf("\n\tpixel format: %s\n", getPixFormat(pixfmt));
+	printf("\n\tpixel format: %s\n", 
+		getPixFormat(format.fmt.pix.pixelformat));
+	printf("\tcolor space: %s\n", 
+		getColorspace(format.fmt.pix.colorspace));
+	printf("\tpixel field: %s\n", 
+		getField(format.fmt.pix.field));
 
 	close(fd);
 	return 0;
